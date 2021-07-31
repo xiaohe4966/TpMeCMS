082a27de78d5aeaaa0319b4fc5ad9b25de0563d3
小和 <496631085@qq.com>
2021-07-31 16:14:01 +0800

    添加附近插件（可以是商家或者人等坐标）

---
+++ addons/redisgeo/config.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+  
+    [
+        'name' => '__tips__',
+        'title' => '温馨提示',
+        'type' => '',
+        'content' => [],
+        'value' => '1、请在开启redis插件，并在config.php里面配置redis的正确配置，使用说明示例:点击《Redis坐标插件》名称或联系<a href="https://he4966.cn/index" target="_blank">https://he4966.cn/index</a><br/><br>'."\n"
+            .'注意是否开启redis插件和配置更改',
+        'rule' => '',
+        'msg' => '',
+        'tip' => '',
+        'ok' => '',
+        'extend' => '',
+    ],
+];
--- application/extra/addons.php
+++ application/extra/addons.php
@@ -2,7 +2,11 @@
 
 return [
     'autoload' => false,
-    'hooks' => [],
+    'hooks' => [
+        'testhook' => [
+            'redisgeo',
+        ],
+    ],
     'route' => [],
     'priority' => [],
 ];
